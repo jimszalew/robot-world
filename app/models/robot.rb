ad6cb414b9f3c2d97deb4a40442cc71614c63bf0
Jim Szalewski <jim.szalewski@gmail.com>
2017-06-27 15:00:04 -0600

    add info to initialize method

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -2,6 +2,10 @@ require 'sqlite3'
 
 class Robot
   def initialize(robot_params)
+    @id         = robot_params["id"]
+    @name       = robot_params["name"]
+    @state      = robot_params["state"]
+    @department = robot_params["department"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
   end
